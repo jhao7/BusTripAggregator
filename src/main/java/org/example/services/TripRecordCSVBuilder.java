b0cb2f5aaed09628d882e789b0275d9c972ec7eb
jinhao <ernest79415@yahoo.com>
2025-03-21 00:42:23 +1100

    Fixed generated csv file name.

--- src/main/java/org/example/services/TripRecordCSVBuilder.java
+++ src/main/java/org/example/services/TripRecordCSVBuilder.java
@@ -98,7 +98,7 @@ public class TripRecordCSVBuilder {
 
     public void generateTripRecordCSV(List<TripRecord> tripRecord) throws IOException {
         try {
-            BufferedWriter writer = Files.newBufferedWriter(Paths.get("GeneratedTripRecords.csv"));
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get("trips.csv"));
 
             CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
                     .setDelimiter(", ")
